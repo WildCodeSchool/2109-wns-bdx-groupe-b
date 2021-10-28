41e2311b9bc84eca37bdba01163625f91e0666d1
Corentin <lecoeurmathieu@icloud.com>
2021-10-28 17:37:03 +0200

    Configuration de typeORM + Connexion à MySQL

--- back/build/index.js
+++ back/build/index.js
@@ -1,1 +1,10 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+require("reflect-metadata");
+var typeorm_1 = require("typeorm");
+(0, typeorm_1.createConnection)().then(function (connection) {
+    // here you can start to work with your entities
+    console.log('connected');
+}).catch(function (error) { return console.log(error); });
+//# sourceMappingURL=index.js.map
